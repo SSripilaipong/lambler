288539071c224d8f4b63232805d0d686443c519e
SSripilaipong <SHSnail@mail.com>
2022-07-01 18:05:26 +0700

    refactor(http.get): refactor test case test_should_call_handler for readability
    Refs: #1

--- tests/test_http/test_get/test_request.py
+++ tests/test_http/test_get/test_request.py
@@ -5,7 +5,6 @@ from lambler.http import HttpEndpoint
 
 
 def test_should_call_handler():
-    lambler = Lambler()
     endpoint = HttpEndpoint()
 
     @endpoint.get("/")
@@ -14,11 +13,7 @@ def test_should_call_handler():
 
     handle.is_called = False
 
-    lambler.use(
-        endpoint,
-    )
-
     with open("./tests/test_http/test_get/event_data/simple.json", "r") as file:
-        lambler(json.load(file), ...)
+        Lambler().use(endpoint)(json.load(file), ...)
 
     assert handle.is_called

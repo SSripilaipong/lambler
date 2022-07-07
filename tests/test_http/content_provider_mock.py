a2c63985e1a774298d5cdccd316fdc90631cb389
SSripilaipong <SHSnail@mail.com>
2022-07-07 22:58:38 +0700

    feat(http.content): test_should_pass_loaded_content_to_endpoint
    Refs: #1

--- tests/test_http/content_provider_mock.py
+++ tests/test_http/content_provider_mock.py
@@ -5,10 +5,13 @@ from lambler.content import ContentProvider
 
 class ContentProviderMock(ContentProvider):
 
-    def __init__(self):
+    def __init__(self, *, load__return=None):
+        self._load__return = load__return
+
         self.load__is_called = False
         self.load__key = None
 
     def load(self, key: str) -> Any:
         self.load__is_called = True
         self.load__key = key
+        return self._load__return
--- tests/test_http/test_content_provider.py
+++ tests/test_http/test_content_provider.py
@@ -95,3 +95,18 @@ def test_should_load_only_from_provider_with_specified_scope():
 
     lambler(simple_get_request(""), ...)
     assert provider1.load__is_called and not provider2.load__is_called
+
+
+def test_should_pass_loaded_content_to_endpoint():
+    api = HttpApi()
+
+    @api.get("")
+    def endpoint(content: str = Content("my-content")):
+        endpoint.content = content
+
+    lambler = Lambler()
+    lambler.use_content(ContentProviderMock(load__return="Hello World!"))
+    lambler.handle(api)
+
+    lambler(simple_get_request(""), ...)
+    assert endpoint.content == "Hello World!"

f06fd35e52152cd8d6b25d12c006e36c21086950
SSripilaipong <SHSnail@mail.com>
2022-07-11 23:12:00 +0700

    release: 0.0.1
    Refs: #1

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 import setuptools
 from setuptools import setup
 
-
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 NAME = 'lambler'
-VERSION = '0.0.0'
+VERSION = '0.0.1'
 URL = 'https://github.com/SSripilaipong/lambler'
 LICENSE = 'MIT'
 AUTHOR = 'SSripilaipong'
@@ -15,14 +14,15 @@ EMAIL = 'SHSnail@mail.com'
 setup(
     name=NAME,
     version=VERSION,
-    packages=[package for package in setuptools.find_packages() if not package.startswith('tests')],
+    packages=[package for package in setuptools.find_packages() if
+              package == "lambler" or package.startswith('lambler.')],
     url=URL,
     license=LICENSE,
     author=AUTHOR,
     author_email=EMAIL,
     description=None,
     long_description=None,
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     install_requires=requirements,
     classifiers=[],
 )

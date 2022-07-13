55f6d839ff7031d85c666880c4320e1868f09f1f
SSripilaipong <SHSnail@mail.com>
2022-07-13 12:17:32 +0700

    release: 0.0.3
    Refs: #1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 NAME = 'lambler'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 URL = 'https://github.com/SSripilaipong/lambler'
 LICENSE = 'MIT'
 AUTHOR = 'SSripilaipong'

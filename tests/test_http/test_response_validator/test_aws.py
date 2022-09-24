61983bedd283047c57546bf094f9fb3f36d41eaf
Santhapon Sripilaipong <santhapon@senestia.com>
2022-09-24 17:34:05 +0700

    feat(lambler.http.response_validator.aws): test_should_return_html_response_from_HtmlResponse_returned_by_endpoint
    Refs: #5

--- tests/test_http/test_response_validator/test_aws.py
+++ tests/test_http/test_response_validator/test_aws.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from lambler.http import AwsHttpResponseValidator, HttpResponse, JsonResponse
+from lambler.http import AwsHttpResponseValidator, HttpResponse, JsonResponse, HtmlResponse
 
 
 def test_should_return_empty_response_with_status_200_by_default():
@@ -46,6 +46,23 @@ def test_should_return_response_from_JsonResponse_returned_by_endpoint():
             "Content-Type": "application/json"
         },
         "body": {"message": "ok", "hello": "world"},
+        "cookies": [],
+        "isBase64Encoded": False,
+    }
+
+
+# noinspection PyPep8Naming
+def test_should_return_html_response_from_HtmlResponse_returned_by_endpoint():
+    validator = AwsHttpResponseValidator()
+
+    raw = HtmlResponse(HTTPStatus.OK, '<html lang="en"></html>')
+
+    assert validator.validate(raw) == {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "text/html; charset=UTF-8"
+        },
+        "body": '<html lang="en"></html>',
         "cookies": [],
         "isBase64Encoded": False,
     }
